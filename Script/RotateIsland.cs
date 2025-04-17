bfb306db0436d7342af1d7fec82e6fcf355f0963
Maximilien Puffet <puffetmaximilien21@gmail.com>
2025-04-17 14:07:04 +0200

    Add island

---
+++ Script/RotateIsland.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RotateIsland : MonoBehaviour
+{
+    [SerializeField] private float _speedRotation;
+    [SerializeField] private float _speedScale;
+    [SerializeField] Vector3 _rotationAxis;
+
+    void Update()
+    {
+        transform.Rotate(_rotationAxis * _speedRotation * Time.deltaTime);
+    }
+
+    public float SineAmount()
+    {
+        return Mathf.Sin(_speedScale * Time.time);
+    }
+}
